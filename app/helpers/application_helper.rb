6fc68cf887e4e8918de9537efb6b798b27e260d6
Travis Johnston <tjohnst1@gmail.com>
2015-11-03 22:08:14 -0800

    add error message partial to the food cart views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flashToAlertClass(key)
+    case key
+      when "message"
+        "alert-success"
+      when "warning"
+        "alert-danger"
+      else
+        "alert-info"
+      end
+  end
 end
