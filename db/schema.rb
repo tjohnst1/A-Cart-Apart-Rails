c06eec0771d5e177e20308fffb507053bdbaabac
Travis Johnston <tjohnst1@gmail.com>
2015-11-03 17:43:22 -0800

    create tag  model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104013518) do
+ActiveRecord::Schema.define(version: 20151104013915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20151104013518) do
   create_table "reviews", force: :cascade do |t|
     t.string   "content"
     t.string   "rating"
+    t.integer  "food_cart_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.integer  "food_cart_id"
     t.integer  "user_id"
     t.datetime "created_at",   null: false
