d380e6fd49652b8cc6b922c0ec6a732eb7abc51b
Travis Johnston <tjohnst1@gmail.com>
2015-11-04 22:52:45 -0800

    update google maps markers to show letter labels

--- app/assets/javascripts/googleMaps.js
+++ app/assets/javascripts/googleMaps.js
@@ -8,12 +8,18 @@ function initMap() {
 
 var foodCarts = gon.food_carts
 
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
 function setMarkers(map) {
   for (var i = 0; i < foodCarts.length; i++) {
     var foodCart = foodCarts[i];
     var marker = new google.maps.Marker({
       position: {lat: Number(foodCart.latitude), lng: Number(foodCart.longitude)},
+      animation: google.maps.Animation.DROP,
+      label: String.fromCharCode("A".charCodeAt(0) + i),
       map: map,
+      zIndex: i
     });
   }
 }
