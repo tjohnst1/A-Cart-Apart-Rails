f61ea9e29b556693f1b069bab5eb80ab4798fe50
Travis Johnston <tjohnst1@gmail.com>
2015-11-10 11:41:30 -0800

    refactor the tag list into a helper function and update the seed file to remove the creation of tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,21 @@ module ApplicationHelper
         "alert-info"
       end
   end
-  def add_query(category)
-    if params[:query]
-      "#{params[:query]}&#{category}"
-    else
-      category
-    end
+  def current_categories
+    [
+     "Afghan", "African", "Albanian", "American", "Argentinian", "Asian", "Australian", "Austrian", "Bagels", "Bakery",
+     "BBQ", "Belgian", "Brazilian", "Breakfast", "British", "Burmese", "Cajun", "Californian", "Calzones", "Cambodian",
+     "Cantonese", "Caribbean", "Cheesesteaks", "Chicken", "Chili", "Chinese", "Classic", "Coffee and Tea", "Colombian",
+     "Costa Rican", "Crepes", "Cuban", "Deli", "Dessert", "Dim Sum", "Diner", "Dinner", "Dominican", "Eclectic",
+     "Ecuadorian", "Egyptian", "El Salvadoran", "Empanadas", "English", "Ethiopian", "Filipino", "French", "Fresh Fruits",
+     "Frozen Yogurt", "German", "Gluten-Free", "Greek", "Grill", "Guatemalan", "Gyro", "Haitian", "Halal",
+     "Hamburgers", "Hawaiian", "Hot Dogs", "Ice Cream", "Indian", "Indonesian", "Irish", "Italian", "Jamaican", "Japanese",
+     "Korean", "Kosher", "Latin American", "Lebanese", "Malaysian", "Mandarin", "Mediterranean", "Mexican", "Middle Eastern",
+     "Mongolian", "Moroccan", "Nepalese", "New American", "Noodles", "Organic", "Pakistani", "Pasta", "Persian", "Peruvian",
+     "Pitas", "Pizza", "Polish", "Portuguese", "Puerto Rican", "Ramen",  "Ribs", "Russian", "Salads", "Sandwiches", "Scandinavian",
+     "Seafood", "Senegalese", "Shakes", "Smoothies and Juices", "Soul Food", "Soup", "South African", "South American",
+     "Southern", "Southwestern", "Spanish", "Steak", "Subs", "Sushi", "Szechwan", "Taiwanese", "Tapas", "Tex-Mex", "Thai",
+     "Tibetan", "Turkish", "Ukrainian", "Vegan", "Vegetarian", "Venezuelan", "Vietnamese", "Wings", "Wraps"
+    ]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,23 +17,3 @@ foodCarts.each do |name, address, phone_number, website, monday_hours, tuesday_h
   newFoodCart.tag_list.add(tags)
   newFoodCart.save
 end
-
-tags = [
- "Afghan", "African", "Albanian", "American", "Argentinian", "Asian", "Australian", "Austrian", "Bagels", "Bakery",
- "BBQ", "Belgian", "Brazilian", "Breakfast", "British", "Burmese", "Cajun", "Californian", "Calzones", "Cambodian",
- "Cantonese", "Caribbean", "Cheesesteaks", "Chicken", "Chili", "Chinese", "Classic", "Coffee and Tea", "Colombian",
- "Costa Rican", "Crepes", "Cuban", "Deli", "Dessert", "Dim Sum", "Diner", "Dinner", "Dominican", "Eclectic",
- "Ecuadorian", "Egyptian", "El Salvadoran", "Empanadas", "English", "Ethiopian", "Filipino", "French", "Fresh Fruits",
- "Frozen Yogurt", "German", "Gluten-Free", "Greek", "Grill", "Guatemalan", "Gyro", "Haitian", "Halal",
- "Hamburgers", "Hawaiian", "Hot Dogs", "Ice Cream", "Indian", "Indonesian", "Irish", "Italian", "Jamaican", "Japanese",
- "Korean", "Kosher", "Latin American", "Lebanese", "Malaysian", "Mandarin", "Mediterranean", "Mexican", "Middle Eastern",
- "Mongolian", "Moroccan", "Nepalese", "New American", "Noodles", "Organic", "Pakistani", "Pasta", "Persian", "Peruvian",
- "Pitas", "Pizza", "Polish", "Portuguese", "Puerto Rican", "Ramen",  "Ribs", "Russian", "Salads", "Sandwiches", "Scandinavian",
- "Seafood", "Senegalese", "Shakes", "Smoothies and Juices", "Soul Food", "Soup", "South African", "South American",
- "Southern", "Southwestern", "Spanish", "Steak", "Subs", "Sushi", "Szechwan", "Taiwanese", "Tapas", "Tex-Mex", "Thai",
- "Tibetan", "Turkish", "Ukrainian", "Vegan", "Vegetarian", "Venezuelan", "Vietnamese", "Wings", "Wraps"
-]
-
-tags.each do |tag|
-  Tag.create(name: tag)
-end
