e04b3425c30a60adcb5931b9601c2d63f535b34d
Travis Johnston <tjohnst1@gmail.com>
2015-11-13 20:42:25 -0800

    add additional styling to both the login and sign up views

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -4,14 +4,15 @@ describe 'authentication pages' do
   before(:each, :login) do
     FactoryGirl.create(:user)
     visit food_carts_path
-    click_on 'Sign In'
+    click_on 'Log In'
     fill_in 'user_login', with: 'testaccount'
     fill_in 'user_password', with: 'testpassword'
     click_on 'Log in'
   end
   it 'allows a user to sign up for an account' do
     visit food_carts_path
-    click_on 'Sign Up'
+    click_on 'Log In'
+    click_on 'Need an Account?'
     fill_in 'user_username', with: 'firstspec'
     fill_in 'user_email', with: 'spec@email.com'
     fill_in 'user_password', with: 'specpassword'
@@ -19,19 +20,19 @@ describe 'authentication pages' do
     click_on 'Sign up'
     expect(page).to(have_content('Welcome! You have signed up successfully.'))
   end
-  it 'allows a user to sign in with a e-mail address' do
+  it 'allows a user to sign in with a e-mail address', js:true do
     FactoryGirl.create(:user)
     visit food_carts_path
-    click_on 'Sign In'
+    click_on 'Log In'
     fill_in 'user_login', with: 'test@email.com'
     fill_in 'user_password', with: 'testpassword'
     click_on 'Log in'
     expect(page).to(have_content('Signed in successfully.'))
   end
-  it 'allows a user to sign in with a username', login: true do
+  it 'allows a user to sign in with a username', login: true, js:true do
     expect(page).to(have_content('Signed in successfully.'))
   end
-  it 'allows a user to sign out', login: true do
+  it 'allows a user to sign out', login: true, js:true do
     click_on 'Sign Out'
     expect(page).to(have_content('Signed out successfully.'))
   end
--- spec/features/filter_pages_spec.rb
+++ spec/features/filter_pages_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 describe 'filter pages' do
-  it 'allows a user to filter food carts by category' do
+  it 'allows a user to filter food carts by category', js:true do
     visit food_carts_path
-    check 'Japanese'
+    click_on 'categories-header-link'
+    page.find('#Japanese-filter').set(true)
     wait 2
     expect(page).to(have_content('Umai Ramen Cart'))
   end
-  it 'allows a user to filter food carts by category' do
+  it 'allows a user to filter food carts by category', js:true do
     visit food_carts_path
-    check 'Southern'
+    click_on '#categories-header-link'
+    page.find('#Southern-filter').set(true)
     wait 2
     expect(page).to_not(have_content('Umai Ramen Cart'))
   end
